300d0f938059dd15da575b3428a365ef1fb3280a
Vinicius Leao <vinicius.s.silva-kee@parceirosedu.com.br>
2023-04-30 13:11:49 -0300

    initial commit

---
+++ src/main/java/br/com/iesb/nicetask/NicetaskApplication.java
@@ -0,0 +1,13 @@
+package br.com.iesb.nicetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicetaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicetaskApplication.class, args);
+	}
+
+}
