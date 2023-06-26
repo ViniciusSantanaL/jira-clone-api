5ac16166610775aaf9607c9f9d54802f06aedefb
Vinicius Leao <vinicius.s.silva-kee@parceirosedu.com.br>
2023-06-25 23:04:43 -0300

    fix: request mapping anotation

--- src/main/java/br/com/iesb/jira/application/incoming/user/controller/UserFetchController.java
+++ src/main/java/br/com/iesb/jira/application/incoming/user/controller/UserFetchController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-@RequestMapping(name = "/api/v1/users")
+@RequestMapping("/api/v1/users")
 @RestController
 public class UserFetchController implements UserFetchApi {
     private final UserFetchService userFetchService;
